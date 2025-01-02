85e492cf5389f9607e1b95931faeb05e0231525e
Sushma2025 <Sushma.Talupula@ibm.com>
2025-01-02 12:51:27 +0530

    Add files via upload

---
+++ Python/Activities/Actiivty8.py
@@ -0,0 +1,13 @@
+#num = [10,20,30,40,50,20]
+#print("Given list is", num)
+
+#firstElement = num[0]
+#lastElement = num[-1]
+
+firstElement = input("Enter first number: ")
+lastElement = input("ENter last number: ")
+
+if (firstElement == lastElement):
+    print("True")
+else:
+    print("False")
