9597569b8641511076c07ac1c6a194d7676c41b0
Sushma2025 <Sushma.Talupula@ibm.com>
2025-03-22 21:46:37 +0530

    Add files via upload

--- Cucumber/Activity6Runner4.java
+++ Cucumber/Activity6Runner4.java
@@ -1,5 +1,20 @@
 package testRunner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	    features = "Features",
+	    glue = {"stepDefinitions"},
+	    tags = "@SmokeTest",
+	    plugin = {"junit:test-reports"},
+	    monochrome = true
+	)
+
+
 public class Activity6Runner4 {
 
 }
